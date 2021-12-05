8efa1a05d6bbe409d5e35f972b41cb99943451b9
ColtMagri <colt.magri@mymail.champlain.edu>
2021-12-05 18:16:41 -0500

    created some initial UI, loaded in SVG into a container frame, started creating functions for psydo user movement

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import floorplan from './ccmfloorplan.svg'
 import './App.css';
 import AstarComponent from './astarComponent.js' 
 
@@ -6,22 +7,36 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div id="interior-map-container">
+        <img id="interior-map" src= {floorplan}/>
+      </div>
+      <input type="button" id="moveup" class="button" value="Move Up"></input>
+      <input type="button" id="movedown" class="button" value="Move Down"></input>
+      <input type="button" id="moveleft" class="button" value="Move Left"></input>
+      <input type="button" id="moveright" class="button" value="Move Right"></input>
       </header>
+
       <AstarComponent />
     </div>
   );
+}
+
+window.onload=function() {
+  var btn = document.getElementById("moveup");
+  btn.addEventListener("click", moveUp)
+}
+
+function moveUp() {
+  var currentMap = document.getElementById("interior-map");
+  var currentTransform = currentMap.style.transform
+  var currentX = ""
+  var currentY= ""
+
+  for (let i = 0; i < currentTransform.length; i++) {
+    
+  }
+
+  currentMap.style.transform = "translate(-50px, -50px)"
 }
 
 export default App;
