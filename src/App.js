559b08f45d4c335947f5c18eae24b8e02c55706e
ColtMagri <colt.magri@mymail.champlain.edu>
2021-12-07 19:18:29 -0500

    finished functionality to move around the map with buttons, included UI that displays current possitional coordinates on the map

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ function App() {
       <input type="button" id="movedown" class="button" value="Move Down"></input>
       <input type="button" id="moveleft" class="button" value="Move Left"></input>
       <input type="button" id="moveright" class="button" value="Move Right"></input>
+      <p id="current-coords-display">Current Coordinates:</p>
+      <p id="current-coords"></p>
       </header>
 
       <AstarComponent />
@@ -22,21 +24,116 @@ function App() {
 }
 
 window.onload=function() {
-  var btn = document.getElementById("moveup");
-  btn.addEventListener("click", moveUp)
+  var btnUp = document.getElementById("moveup");
+  btnUp.addEventListener("click", moveUp)
+  var btnDown = document.getElementById("movedown");
+  btnDown.addEventListener("click", moveDown)
+  var btnLeft = document.getElementById("moveleft");
+  btnLeft.addEventListener("click", moveLeft)
+  var btnRight = document.getElementById("moveright");
+  btnRight.addEventListener("click", moveRight)
+
+  var currentMap = document.getElementById("interior-map");
+  currentMap.style.transform = "translate(-30px, -500px)"
+
+  displayCoords()
+}
+
+function displayCoords() {
+  var coordsDisplay = document.getElementById("current-coords");
+  var coordsText = "X: "
+  coordsText += getXandY()[0]
+  coordsText += " Y: "
+  coordsText += getXandY()[1]
+  coordsDisplay.innerText=coordsText
+}
+
+function getXandY() {
+  var currentMap = document.getElementById("interior-map");
+  var currentTransform = currentMap.style.transform
+  currentTransform = currentTransform.substr(9, currentTransform.length - 1)
+  var currentX = ""
+  var currentY= ""
+  var currentChar = ""
+  var charCounter = 0
+  var currentXInt = 0
+  var currentYInt = 0
+
+  while (currentChar != ",") {
+    currentChar = currentTransform.charAt(charCounter)
+    charCounter += 1
+    currentX += currentTransform.charAt(charCounter)
+  }
+  currentX = currentX.slice(0, -4)
+  currentChar = ""
+  while (currentChar != ")") {
+    currentChar = currentTransform.charAt(charCounter)
+    charCounter += 1
+    currentY += currentTransform.charAt(charCounter)
+  }
+  currentY = currentY.slice(0, -3)
+
+  currentXInt = parseInt(currentX)
+  currentYInt = parseInt(currentY)
+
+  var finalCoords = [currentXInt, currentYInt]
+  return finalCoords
 }
 
 function moveUp() {
   var currentMap = document.getElementById("interior-map");
-  var currentTransform = currentMap.style.transform
-  var currentX = ""
-  var currentY= ""
+  var finalTransform = "translate("
+  var currentCoords = getXandY()
+  var currentX = currentCoords[0]
+  var currentY = currentCoords[1]
 
-  for (let i = 0; i < currentTransform.length; i++) {
-    
-  }
+  currentY += 10
+  finalTransform += currentX + "px, " + currentY + "px)"
 
-  currentMap.style.transform = "translate(-50px, -50px)"
+  currentMap.style.transform = finalTransform
+  displayCoords()
+}
+
+function moveDown() {
+  var currentMap = document.getElementById("interior-map");
+  var finalTransform = "translate("
+  var currentCoords = getXandY()
+  var currentX = currentCoords[0]
+  var currentY = currentCoords[1]
+
+  currentY -= 10
+  finalTransform += currentX + "px, " + currentY + "px)"
+
+  currentMap.style.transform = finalTransform
+  displayCoords()
+}
+
+function moveLeft() {
+  var currentMap = document.getElementById("interior-map");
+  var finalTransform = "translate("
+  var currentCoords = getXandY()
+  var currentX = currentCoords[0]
+  var currentY = currentCoords[1]
+
+  currentX += 10
+  finalTransform += currentX + "px, " + currentY + "px)"
+
+  currentMap.style.transform = finalTransform
+  displayCoords()
+}
+
+function moveRight() {
+  var currentMap = document.getElementById("interior-map");
+  var finalTransform = "translate("
+  var currentCoords = getXandY()
+  var currentX = currentCoords[0]
+  var currentY = currentCoords[1]
+
+  currentX -= 10
+  finalTransform += currentX + "px, " + currentY + "px)"
+
+  currentMap.style.transform = finalTransform
+  displayCoords()
 }
 
 export default App;
