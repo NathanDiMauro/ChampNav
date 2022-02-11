8a50419d4a859c8cea05d6d8d759d100b4f0e64d
Nathan DiMauro <nathandimauro1@gmail.com>
2022-02-11 11:35:21 -0500

    Prototype fine tuning

--- src/outdoor.js
+++ src/outdoor.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Map, Marker, InfoWindow, GoogleApiWrapper, Polyline, Polygon } from 'google-maps-react';
+import { Map, Marker, InfoWindow, GoogleApiWrapper, Polyline} from 'google-maps-react';
 import { locations } from "./Locations"
 import axios from 'axios';
 
